0c00a4231e04d7f66f8c3c16209cfc983d1a7a9c
Xavier Lowmiller <xaver.lohmueller@me.com>
2020-03-24 21:01:18 +0100

    Remove print statement

--- Sources/AppParserKit/Android/ApkParser.swift
+++ Sources/AppParserKit/Android/ApkParser.swift
@@ -3,7 +3,7 @@ import Foundation
 public enum ApkParser {
 	public static func parseMetadata(at path: String, aaptPath: String? = nil) throws -> AppMetadata {
 		let aaptOutput = aapt(path, aaptPath: aaptPath)
-print(aaptOutput)
+
 		return try AppMetadata(from: aaptOutput)
 	}
 }
