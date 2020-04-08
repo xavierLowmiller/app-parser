130c4a0d050042da48e87c622e87422448c83590
Xavier Lowmiller <xaver.lohmueller@me.com>
2020-04-08 07:52:20 +0200

    Fill out the rest of attribute string resolution

--- Sources/AppParserKit/Android/AXMLtoXML.swift
+++ Sources/AppParserKit/Android/AXMLtoXML.swift
@@ -92,21 +92,50 @@ private extension Array where Element == UInt8 {
 			let key = strings[Int(self.key)]
 			let value: String
 			switch type {
+			// Null
+			case 0:
+				value = ""
 			// Reference
 			case 1:
 				if data >> 24 == 1 {
-					value = "@android:" + String(data, radix: 16, uppercase: true)
+					value = String(format: "@android:%08X", data)
 				} else {
-					value = "@" + String(data, radix: 16, uppercase: true)
+					value = String(format: "@%08X", data)
+				}
+			// Attribute
+			case 2:
+				if data >> 24 == 1 {
+					value = String(format: "?android:%08X", data)
+				} else {
+					value = String(format: "?%08X", data)
 				}
 			// String
 			case 3:
 				value = strings[Int(self.value)]
+			// Float
+			case 4:
+				value = "\(Float(bitPattern: self.value))"
+			// Dimension
+			case 5:
+				let radixTable: [Float] = [0.00390625, 3.051758E-005, 1.192093E-007, 4.656613E-010]
+				let dimensions = ["px", "dip", "sp", "pt", "in", "mm", "", ""]
+				let dimension = dimensions[Int(data & 0x0f)]
+				let amount = Float(data & 0xffffff00) * radixTable[Int((data >> 4) & 0x03)]
+				value = String(format: "%f", amount) + dimension
+			// Hex
+			case 17:
+				value = String(format: "0x%08x", data)
 			// Bool
 			case 18:
 				value = data == 0 ? "false" : "true"
+			// Colors
+			case 28..<32:
+				value = String(format: "#%08x", data)
+			// Integers
+			case 16..<32:
+				value = String(format: "%d", data)
 			default:
-				value = "\(data)"
+				value = String(format: "<0x%x, type 0x%02x>", data, type)
 			}
 
 			let prefixedKey = key
